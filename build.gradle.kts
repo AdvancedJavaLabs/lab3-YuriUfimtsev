9b62fd07d29504ef0d834835dd1472ccf0c19b2a
Yuri Ufimtsev <ufimtsev3007@gmail.com>
2025-12-08 16:38:48 +0300

    build: update hadoop env, build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -16,5 +16,19 @@ repositories {
 }
 
 dependencies {
+    implementation("org.apache.hadoop:hadoop-common:3.2.1")
+    implementation("org.apache.hadoop:hadoop-mapreduce-client-core:3.2.1")
+    implementation("org.apache.hadoop:hadoop-mapreduce-client-jobclient:3.2.1")
+}
 
+tasks.jar {
+    archiveBaseName.set("sales-analyzer-job")
+    archiveVersion.set("")
+    archiveClassifier.set("")
+
+    manifest {
+        attributes(
+            "Main-Class" to "com.yu_ufimtsev.itmo.sales_data_analysis.SalesPipelineJob"
+        )
+    }
 }
